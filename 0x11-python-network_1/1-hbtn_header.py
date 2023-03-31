32cff21d74bf8b5dc492920ca88a72e92be02b1d
Adepoju Lateef <ademustexcel@gmail.com>
2023-03-31 22:16:01 +0100

    reduced module documentation task 1 again3

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,61 +7,12 @@ variable found in the response.
 
 
 import sys
-import urllib.request
+from urllib.request import urlopen
 import urllib.error
 
 
-def get_x_request_id(url):
-    """
-    Sends an HTTP GET request to the given URL
-    and returns the value of the X-Request-Id
-    header variable.
-
-
-    Args:
-        url (str): The URL to send the request to.
-
-
-    Returns:
-        str: The value of the X-Request-Id header variable.
-
-
-    Raises:
-        urllib.error.URLError: If the request fails due to a network error.
-        ValueError: If the response does not contain the X-Request-Id
-        header variable.
-
-    """
-
-    try:
-        with urllib.request.urlopen(url) as response:
-            x_request_id = dict(response.headers).get('X-Request-Id')
-            if x_request_id is None:
-                raise ValueError("Response does not contain the\
-                                 X-Request-Id header variable.")
-            # print(x_request_id)
-            return x_request_id
-    except urllib.error.URLError as e:
-        raise e
-
-
 if __name__ == "__main__":
-    # Check that the script was called with the correct number of arguments
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} URL")
-        print(f"For example: {sys.argv[0]} https://example.com")
-        sys.exit(1)
-
-    # Get the URL from the command-line argument
     url = sys.argv[1]
 
-    # Get the value of the X-Request-Id header variable
-    try:
-        x_request_id = get_x_request_id(url)
-        print(x_request_id)
-    except urllib.error.URLError as e:
-        print(f"Failed to get X-Request-Id: {e}")
-        sys.exit(1)
-    except ValueError as e:
-        print(f"Failed to get X-Request-Id: {e}")
-        sys.exit(1)
+    with urlopen(url) as res:
+        print(dict(res.headers).get('X-Request-Id'))
