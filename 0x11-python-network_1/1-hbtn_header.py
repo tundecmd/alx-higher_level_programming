8d01d5658a73ca580e0c49b820ec9fe6273e53e6
Adepoju Lateef <ademustexcel@gmail.com>
2023-03-31 22:55:17 +0100

    reduced module documentation task 1 again4

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 """
-A script that takes a URL, sends a request to the URL,
-and displays the value of the X-Request-Id header
-variable found in the response.
+Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./1-hbtn_header.py <URL>
 """
-
-
 import sys
-from urllib.request import urlopen
-import urllib.error
+import urllib.request
 
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./1-hbtn_header.py <URL>")
+        sys.exit(1)
+
     url = sys.argv[1]
 
-    with urlopen(url) as res:
-        print(dict(res.headers).get('X-Request-Id'))
+    try:
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as response:
+            x_request_id = dict(response.headers).get("X-Request-Id")
+            if x_request_id is not None:
+                print(x_request_id)
+            else:
+                print("X-Request-Id header not found in response.")
+    except urllib.error.URLError as e:
+        print("Error opening URL:", e.reason)
+        sys.exit(1)
